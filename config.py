e0c50f3233a57ce213c6f2762048220d12813bdc
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-09 08:08:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+app_id = int(os.environ.get("API_ID", 12345))
+app_key = os.environ.get('API_HASH')
+token = os.environ.get('BOT_TOKEN')
+
+bot=Client(
+           ":memory:",
+           api_id,
+           api_hash,
+           bot_token
+)
