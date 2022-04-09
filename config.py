44201026c05590a3449292019e4e3367ff306e43
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-09 08:18:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,28 @@
-import os
-from pyrogram import Client
+class Config:
+    TOKEN=os.environ['BOT_TOKEN']
+    APP_HASH=os.environ['API_HASH']
+    APP_ID=int(os.environ['API_ID'])
 
-api_id = int(os.environ.get("API_ID", 12345))
-api_hash = os.environ.get('API_HASH')
-bot_token = os.environ.get('BOT_TOKEN')
+    if not TOKEN:
+        raise ValueError(' BOT TOKEN not set')
+    
+    if not APP_HASH:
+        raise ValueError("APP_HASH not set, set it first")
+
+    if not APP_ID:
+        raise ValueError("APP_ID not set, set it first")
+
+    
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 bot=Client(
            ":memory:",
-           api_id,
-           api_hash,
-           bot_token
+           api_id=Config.APP_ID,
+           api_hash=Config.APP_HASH,
+           bot_token=Config.TOKEN
 )
